08276cf618bc333b20461e4705f660ddb79d81f0
Tharunkumar <tharunkumarsk@gmail.com>
2019-11-22 18:23:48 +0100

    Add reducer midleware in index file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './Components/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./Components/App";
+import reducer from "./reducers";
+import middleware from "./middleWare";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducer, middleware);
 
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
--- src/middleWare/index.js
+++ src/middleWare/index.js
@@ -1,10 +1,10 @@
 import logger from "./logger";
+import thunk from "redux-thunk";
 import checker from "./checker";
 import { applyMiddleware } from "redux";
-import thunk from "redux-thunk";
 
 export default applyMiddleware({
   thunk,
-  logger,
-  checker
+  checker,
+  logger
 });
